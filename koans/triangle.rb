edf87f114b90370a394cd0c3a7a4d28961f02e93
mat-jar <mtu07@poczta.fm>
2022-05-17 14:16:37 +0200

    3

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,12 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  #raise TriangleError, "why the exception happened"
+
+  a, b, c = [a, b, c].sort
+  raise TriangleError, "Not a valid triangle" if a <= 0 || a + b <= c
+
   if a == b && b == c
     return :equilateral
   elsif a==b || b==c || a==c
     return :isosceles
   else
-    return :scalene  
+    return :scalene
 
   end
 end
@@ -27,3 +32,19 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+=begin
+
+
+def triangle(a, b, c)
+  [a, b, c].permutation do |sides|
+    raise TriangleError unless sides[0] + sides[1] > sides[2]
+  end
+  case [a,b,c].uniq.size
+    when 3; :scalene
+    when 2; :isosceles
+    when 1; :equilateral
+  end
+end
+
+=end
